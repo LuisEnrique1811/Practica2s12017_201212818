8cbed523b4b9e9633ec921c919f7f2b14e531ef4
l_enr <l_enr@VAIO>
2017-02-28 18:45:28 -0600

    Menu Opciones

--- src/practica2/Practica2.java
+++ src/practica2/Practica2.java
@@ -2,10 +2,11 @@
 package practica2;
 
 public class Practica2 {
-
     
     public static void main(String[] args) {
-       
+        PanelPrincipal ver = new PanelPrincipal();
+        ver.setVisible(true);
+        ver.setLocationRelativeTo(null); 
     }
     
 }
